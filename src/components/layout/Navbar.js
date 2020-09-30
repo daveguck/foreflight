083762b763a49a285b2bdcf4d1d3ddf99b90c766
daveguck <davidguck@yahoo.com>
2020-09-30 16:38:21 -0500

    initial navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,31 +2,33 @@ import React from 'react';
 
 export const Navbar = () => {
   return (
-    <header className='header'>
-      <div className='header__container'>
-        <img
-          src='/images/foreflightLogo.png'
-          alt='foreflight logo'
-          className='header__logo'
-        />
-        <form action='#' class='search'>
-          <input
-            type='text'
-            class='search__input'
-            placeholder='Search Airports'
+    <div className='container'>
+      <header className='header'>
+        <div className='header__container'>
+          <img
+            src='/images/foreflightLogo.png'
+            alt='foreflight logo'
+            className='header__logo'
           />
-          {/* <button class="search__button">
-                        <svg class="search__icon">
-                            <use xlink:href="img/sprite.svg#icon-magnifying-glass"></use>
-                        </svg>
-                    </button> */}
-        </form>
-        <nav className='user-nav'>
-          <span className='user-nav__item'>Home</span>
-          <span className='user-nav__item'>About</span>
-        </nav>
-      </div>
-    </header>
+          <form action='#' class='search'>
+            <input
+              type='text'
+              class='search__input'
+              placeholder='Search Airports'
+            />
+            {/* <button class="search__button">
+                      <svg class="search__icon">
+                          <use xlink:href="img/sprite.svg#icon-magnifying-glass"></use>
+                      </svg>
+                  </button> */}
+          </form>
+          <nav className='user-nav'>
+            <span className='user-nav__item'>Home</span>
+            <span className='user-nav__item'>About</span>
+          </nav>
+        </div>
+      </header>
+    </div>
   );
 };
 
