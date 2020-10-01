1b102cb70de3af1cb673f199ed2f9acab24044b1
daveguck <davidguck@yahoo.com>
2020-10-01 15:41:52 -0500

    setup context API

---
+++ src/context/conditions/ConditionsState.js
@@ -0,0 +1,4 @@
+import React, { useReducer } from 'react';
+import ConditionsContext from './conditionsContext';
+import ConditionsReducer from './conditionsReducer';
+import { GET_CONDITIONS } from '../types';
---
+++ src/context/types.js
@@ -0,0 +1,3 @@
+export const GET_AIRPORT = 'GET_AIRPORT';
+export const GET_CONDITIONS = 'GET_CONDITIONS';
+export const SET_LOADING = 'SET_LOADING';
